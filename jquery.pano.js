6a27979092a98b1a889d8b3ec8634ac2d0a43e53
Dave Kim <innerb@gmail.com>
2011-04-27 18:04:46 -0700

    using tiles in fixed locations. move moving elements to 'outer_hole'

--- jquery.pano.js
+++ jquery.pano.js
@@ -1,25 +1,54 @@
 (function($){
   // like horzontally scrolling tiles
   $(function(){
-	var inner_hole = $('#my-panoflo.inner_hole');
+	var inner_hole = $('div.inner_hole');
  	var inner_hole_width = inner_hole.width();
-	for(var i = 0; i < 55; i++){
-	  $('<div class="tile"/>').css({
-		'position':'absolute',
-		'float':'left',
-		'left':inner_hole_width/55 * i
-	  }).append(
-		$('<img style="width:100%" src="wn_nav.png"/>')
-	  ).appendTo(inner_hole);
+ 	var inner_hole_left = inner_hole.offset().left;
+	var tile = $('img.tile');
+	for(var i = 0; i < 11; i++){
+	  var r = tile.clone().css({
+		'position':'relative',
+		'visibility': 'visible',
+//		'left':inner_hole_width/11 * i,
+	  }).attr({
+		'src':'img/Photo ' + i + '.jpg'
+	  }).appendTo(inner_hole);
 	}
-	var tiles = $('div.tile', inner_hole);
+	var tiles = $('img.tile');
 	tiles.mouseenter(function(e){
-	  $(this)
-	  .siblings()
-	  .removeClass('prev next hover').end()
-	  .prev().addClass('prev').end()
-	  .next().addClass('next').end()
-	  .removeClass('prev next').addClass('hover')
-	});
+	  var hover = $(this);
+	  hover.css({
+		'border':'1px solid magenta'
+	  });
+	  var dst_src_map = {
+		'img.hover':{'elem':hover},
+		'img.left':{'elem':hover.prev()},
+		'img.right':{'elem':hover.next()}
+	  };
+	  var hover_left = hover.offset().left;
+	  for(var dst_key in dst_src_map){
+		var dst_elem = $(dst_key);
+		var src = dst_src_map[dst_key];
+		if(src.elem.length){
+		  dst_elem.attr({
+			'src':src.elem.attr('src')
+		  }).css({
+			'visibility':'visible',
+			'left':hover_left - inner_hole.offset().left,
+		  })
+		} else {
+		  dst_elem.attr({
+			'src':'wn_nav.png'
+		  }).css({
+			'visibility':'hidden'
+		  });
+		}
+	  }
+	}).mouseleave(function(e){
+	  var hover = $(this);
+	  hover.css({
+		'border':'none'
+	  });
+	})
   });
 })(jQuery);
