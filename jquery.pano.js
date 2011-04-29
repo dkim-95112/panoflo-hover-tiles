70867f18fbd6b9da263f509537d8385a9571721d
Dave Kim <innerb@gmail.com>
2011-04-29 04:45:08 -0700

    using einstien pics. feeling close to releasing for resume repitoire

--- jquery.pano.js
+++ jquery.pano.js
@@ -1,48 +1,50 @@
 (function($){
-  // like horzontally scrolling tiles
+  // like horizontally scrolling tiles
   $(function(){
 	var inner_hole = $('div.inner_hole');
  	var inner_hole_width = inner_hole.width();
-	for(var i = 0; i < 11; i++){
+	for(var i = 0; i < 17; i++){
 	  $('<div class="tile"/>').css({
-		'position':'absolute',
-		'left':inner_hole_width/11 * i,
+		'position':'relative',
+		'float':'left',
 	  }).appendTo(inner_hole);
 	}
 	var div_tiles = $('div.tile', inner_hole);
 	var target_imgs = div_tiles.map(function(i){
 	  return $('<img>').attr({
-		'src':'img/Photo ' + i + '.jpg'
+		'src':'img/images-' + i + '.jpeg'
 	  }).css({
-		'position':'absolute',
+		'position':'relative',
 		'width':33,
 	  }).appendTo(this)[0];
 	});
 	var helper_imgs = div_tiles.map(function(){
 	  return $(this).find('img').clone().addClass('helper').css({
-		'top':33,
-	  }).hide().prependTo(this)[0];
+		'position':'absolute',
+		'z-index':-1
+	  }).prependTo(this)[0];
 	});
-	helper_imgs.bind('hover_over', function(){
+	helper_imgs
+	.bind('hover_over', function(){
 	  $(this).show().animate({
-		'width':222, 'margin-left':0
-	  }, 'fast');
+		'width':222, 'margin-left':0, 'top':44}, 'fast');
 	}).bind('hover_prev', function(){
 	  $(this).show().animate({
-		'width':99, 'margin-left':-77
-	  }, 'fast');
-	}).bind('hover_prev_other', function(){
-	  $(this).hide();
+		'width':99, 'margin-left':-55, 'top':44}, 'fast');
 	}).bind('hover_next', function(){
 	  $(this).show().animate({
-		'width':99, 'margin-left':199
-	  }, 'fast');
+		'width':99, 'margin-left':188, 'top':44}, 'fast')
+	}).bind('hover_prev_other', function(){
+	  $(this).hide().css({
+		'width':33, 'margin-left':0, 'top':0
+	  });
 	}).bind('hover_next_other', function(){
-	  $(this).hide();
+	  $(this).hide().css({
+		'width':33, 'margin-left':0, 'top':0
+	  });
 	});
 	target_imgs.mouseenter(function(e){
 	  $(this).css({
-		'border':'1px solid magenta'
 	  }).parent().find('.helper').trigger('hover_over').end()
 	  .prev().find('.helper').trigger('hover_prev').end()
 	  .prevAll().find('.helper').trigger('hover_prev_other').end().end().end()
@@ -50,7 +52,6 @@
 	  .nextAll().find('.helper').trigger('hover_next_other').end().end().end()
 	}).mouseleave(function(e){
 	  $(this).css({
-		'border':'none'
 	  });
 	});
   });
